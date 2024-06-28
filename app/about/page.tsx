ce10f60d4805313b40f42f4c671966c1b89be4bd
sreeharsha-rav <dev.sreeharsha@gmail.com>
2024-06-27 19:34:24 -0700

    update done

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,9 +10,9 @@ return (
         <div className="pt-5 grid grid-cols-1 md:grid-cols-2 gap-4">
             <div className="flex flex-col gap-2">
                 <Experience />
-                <Education />
             </div>
             <div className="flex flex-col gap-2">
+                <Education />
                 <Skills />
                 {/* TODO: <Certifications /> */}
             </div>
