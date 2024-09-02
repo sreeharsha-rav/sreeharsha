c8361ae677f8ab8e561e96c3d6dc25630f131fe2
sreeharsha-rav <dev.sreeharsha@gmail.com>
2024-09-02 06:43:07 -0700

    chore: remove nextjs files for astro

--- components/projects/opensource.tsx
+++
@@ -1,8 +0,0 @@
-export default function OpenSource() {
-    return (
-        <div>
-            <h2 className="text-xl font-bold">Open Source</h2>
-            {/* TODO */}
-        </div>
-    );
-}
