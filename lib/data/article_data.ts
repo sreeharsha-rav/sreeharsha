c49a868488de689c56198c2a7c2e87c5e5ddac83
Sreeharsha Raveendra <23069128+sreeharsha-rav@users.noreply.github.com>
2024-09-03 03:50:56 -0700

    docs: update article data Sept 3rd 2024

--- lib/data/article_data.ts
+++ lib/data/article_data.ts
@@ -7,6 +7,24 @@ interface Article {
 
 const articles_data: Article[] = [
     {
+        title: "NeoChat: A Context-Aware RAG Chatbot Starter",
+        url: "https://dev.to/sraveend/neochat-a-context-aware-rag-chatbot-starter-kit-19bf",
+        tags: ["typescript", "RAG", "AI", "Langchain", "Vue", "Full-Stack"],
+        date: "Sept 2024",
+    },
+    {
+        title: "The Power of Pipes: Streamlining Data Flow in Backend Development",
+        url: "https://dev.to/sraveend/the-power-of-pipes-streamlining-data-flow-in-backend-development-nhh",
+        tags: ["typescript", "nodejs", "backend"],
+        date: "Aug 2024",
+    },
+    {
+        title: "Building a Scalable AI Translator with Next.js Pt 1",
+        url: "https://dev.to/sraveend/building-a-scalable-ai-translator-with-nextjs-pt-1-leveraging-composition-and-custom-hooks-gla",
+        tags: ["react", "nextjs", "frontend", "ai"],
+        date: "Aug 2024",
+    },
+    {
         title: "Basic React Design Patterns: A practical look with Tic Tac Toe",
         url: "https://dev.to/sraveend/basic-react-design-patterns-a-practical-look-with-tic-tac-toe-3c76",
         tags: ["react", "design patterns", "frontend", "typeScript"],
